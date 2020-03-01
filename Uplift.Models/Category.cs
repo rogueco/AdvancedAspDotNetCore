fb290e57c1f25fbbf28c68fdb1fdefc7fc8de7af
TomFletcherPersonal <tom@tom-fletcher.co.uk>
2020-03-01 10:30:47 +0000

    Got it to run.

---
+++ Uplift.Models/Category.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Uplift.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name="Category Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name="Display Order")]
+        public int DisplayOrder { get; set; }
+
+    }
+}
